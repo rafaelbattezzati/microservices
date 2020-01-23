72f6c43a0ade6519c0972ff727f33c6a923680cf
Rafael BATTEZZATI <battraf@s4dad.aws.cloud.tech.ec.europa.eu>
2020-01-23 11:23:45 +0100

    Microservices example

---
+++ Ex_Files_Mastering_Microservices_Java/Exercise Files/Chapter 7/OTRS_SERVICES/restaurant-service/src/test/java/com/packtpub/mmj/restaurant/resources/docker/DockerIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.packtpub.mmj.restaurant.resources.docker;
+
+/**
+ *
+ * @author
+ */
+public interface DockerIntegrationTest {
+    // Marker for Docker integratino Tests
+}
---
+++ Ex_Files_Mastering_Microservices_Java/Exercise Files/Chapter 7/OTRS_SERVICES/user-service/src/test/java/com/packtpub/mmj/user/resources/docker/DockerIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.packtpub.mmj.user.resources.docker;
+
+/**
+ *
+ * @author
+ */
+public interface DockerIntegrationTest {
+    // Marker for Docker integratino Tests
+}
